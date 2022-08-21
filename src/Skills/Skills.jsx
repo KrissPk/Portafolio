5c89e5ca249abcfcd14590467f45fa24bd6c3999
KrissPk <ils.tsmr.cristian@gmail.com>
2022-08-21 19:56:16 +0200

    ++

--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -7,6 +7,14 @@ export function SkillsGrid() {
         <img className={styles.imgHtml} src="./img/html.svg" alt="HTML" />
         <h3 className={styles}>Html 5</h3>
       </li>
+      <li className={styles.li1}>
+        <img className={styles.imgHtml} src="./img/css.svg" alt="HTML" />
+        <h3 className={styles}>CSS 3</h3>
+      </li>
+      <li className={styles.li1}>
+        <img className={styles.imgHtml} src="./img/javascript.svg" alt="HTML" />
+        <h3 className={styles}>JavaScript</h3>
+      </li>
     </ul>
   );
 }
